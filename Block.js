649a73e01c4f38a57db24f3e2260f298c9d843c9
iisxrxh <74046058+iisxrxh@users.noreply.github.com>
2021-05-03 17:01:00 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,12 +1,12 @@
 class Block{
   constructor(x, y, width, height) {
-    this.image = loadImage("block.png");
       var options = {
           restitution :0.4,
           friction :0.0,
          
       }
-      super(x,y,height, width);
+      this.body = Bodies.rectangle(x, y, width,height, options)
+      this.image = loadImage("block.png");
       this.width = width;
       this.height = height;
       this.Visibility = 225;
@@ -15,16 +15,21 @@ class Block{
     }
     display(){
       console.log(this.body.speed)
-      if(this.body.speed>3){
-       display(Block)
+      if(this.body.speed<3){
+        var angle = this.body.angle
+        var pos = this.body.position
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image,0,0, this.height, this.width)
+        pop()
       }
      else{
         World.remove(world, this.body)
         push()
         this.Visibility -=5
-        tint(255,this.Visibility)
-        image(this.image,this.body.position.x, this.body.position.y, this.height, this.width)
-        pop()
+       pop()
       }
     }
 }
